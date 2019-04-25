d07d9ce276ee26a79152565b90b61b9e5e6a846a
LM Bonnefont <louis_marie.bonnefont@edu.escpeurope.eu>
2019-04-25 18:21:20 +0200

    boucle display/creation employee fermée

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Employee.destroy_all
+
+User.create!(email: "jeanbon@gmail.com", password: "123456")
+Employee.create!(name: "Bonnefont", surname:"LM", position:"PO", summary: "Bon gars sur")
+
