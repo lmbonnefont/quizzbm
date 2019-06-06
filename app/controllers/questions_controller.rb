86cc9055b99006045a76ddaff4e463cb71984091
louismarie bonnefont <louis_marie.bonnefont@edu.escpeurope.eu>
2019-06-06 22:46:17 +0200

    question créée

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,29 +1,47 @@
 class QuestionsController < ApplicationController
-  before_action :questions_available?
 
-  def questions_available?
-    if current_user.answered_questions.length == Question.all.length
-      flash[:error] = "You have already answered all the questions. Ask the King of ragots for more secrets!"
-      redirect_to employees_path
-    end
-  end
 
 
   def show
-    @user = current_user
-    answered = @user.answered_questions
-    questions = Question.where.not(id: answered) #On trouve une question qui n'a pas encore été répondue par le user
-    @question = questions.sample
-    @user.current_question = @question.id #On store la question à laquelle est le user pour pouvoir la retrouver dans la méthode check ensuite
-    @user.save
+    if questions_available?
+      @user = current_user
+      answered = @user.answered_questions
+      questions = Question.where.not(id: answered) #On trouve une question qui n'a pas encore été répondue par le user
+      @question = questions.sample
+      @user.current_question = @question.id #On store la question à laquelle est le user pour pouvoir la retrouver dans la méthode check ensuite
+      @user.save
 
-    @answer = Answer.new
+      @answer = Answer.new
 
-    employees = Employee.where.not(id: @question.correct_answer.id) #On sélectionne les employees qui ne sont pas la bonne réponse à la question
-    employee = employees.sample
-    @possible_answers = ["#{@question.correct_answer.surname} #{@question.correct_answer.name}", "#{employee.surname} #{employee.name}"]
-    @possible_answers.shuffle! #On mélange le tableau pour que la bonne réponse ne soit pas ts en 1ere position
+      employees = Employee.where.not(id: @question.correct_answer.id) #On sélectionne les employees qui ne sont pas la bonne réponse à la question
+      employee = employees.sample
+      @possible_answers = ["#{@question.correct_answer.surname} #{@question.correct_answer.name}", "#{employee.surname} #{employee.name}"]
+      @possible_answers.shuffle! #On mélange le tableau pour que la bonne réponse ne soit pas ts en 1ere position
+
+    else
+      flash[:error] = "You have already answered all the questions. Ask the King of ragots for more secrets!"
+      redirect_to employees_path
+      end
+
+
   end
+
+  def new
+    @question = Question.new
+    @employee = Employee.all
+  end
+
+  def create
+    question = Question.new
+    question.label =  question_params["label"]
+    question.correct_answer =  Employee.find(question_params["correct_answer"])
+    question.save
+    flash[:correct] = "Question created"
+    redirect_to employees_path
+  end
+
+
+
 
   def check
     answer_user = params[:answer][:employee]
@@ -43,6 +61,20 @@ class QuestionsController < ApplicationController
     current_user.save
 
     redirect_to question_path(1)
+  end
+
+private
+
+  def questions_available?
+    if current_user.answered_questions.length == Question.all.length
+      return false
+    else
+      return true
+    end
+  end
+
+  def question_params
+    params.require(:question).permit(:label, :correct_answer)
   end
 
 end
